0b8f5102aaa8e8ba2ea4d5616cb4755542c328ec
Kurtić Džemal <Dzemaludeen@gmail.com>
2026-09-08 11:58:42 +0200

    Add Product

---
+++ CoffeeShop.PointOfSale.EntityFramework/ProductContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CoffeeShop.PointOfSale.EntityFramework;
+
+internal class ProductContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+        optionsBuilder.UseSqlite($"Data Source = products.db");
+}
